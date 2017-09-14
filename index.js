b1c25863e0207132e32ad7b8afb19ab423936230
Faith <fmugure@intracitygeeks.org>
2017-09-14 04:21:05 +0000

    update js

--- index.js
+++ index.js
@@ -3,40 +3,53 @@ const RED = "RED";
 const BLUE = "BLUE";
 const YELLOW = "YELLOW";
 const GREEN = "GREEN";
+
 var simon = {
-	sendColor: function(color) {
-		if (!simon.sequence.length) {
-			simon.nextSequence();
-		} else {
-			if (color === simon.sequence[simon.step]) {} else {
-				alert("WRONG!!");
-				simon.sequence = [];
-				simon.step = 0;
-			}
-		}
-		console.log("NEW COLOR: " + color);
-	},
-	sequence: [],
-	colors: [RED, BLUE, YELLOW, GREEN],
-	step: 0,
-	nextSequence: function() {
-		var nextColor = simon.colors[Math.floor(Math.random() simon.colors.length)];
-		simon.sequence.push(nextColor);
-		console.log("the sequence", simon.sequence);
-	}
+  sendColor: function(color){
+    
+    if(!simon.sequence.length){
+      // start a new game
+      simon.nextSequence();
+    }
+    else {
+      
+      if(color === simon.sequence[simon.step]){
+        // go to next step
+        if(simon.step === simon.sequence.length - 1){
+          console.log("sequence complete!");
+          simon.step = 0;
+          simon.nextSequence();
+        }
+        else {
+          simon.step++;
+        }
+        
+      }
+      else {
+        // !!lose condition
+        alert("WRONG!!");
+        simon.sequence = [];
+        simon.step = 0;
+      }
+      
+    }
+    
+    console.log("NEW COLOR: " + color);
+  },
+  sequence: [],
+  colors: [RED, BLUE, YELLOW, GREEN],
+  step: 0,
+  nextSequence: function(){
+    var nextColor = simon.colors[Math.floor( Math.random() * simon.colors.length )];
+    simon.sequence.push(nextColor);
+    console.log("the sequence ", simon.sequence);
+  }
 };
-$(document).ready(function() {
-	$("#red").click(function() {
-		simon.sendColor(RED)
-	});
-	$("#blue").click(function() {
-		simon.sendColor(BLUE)
-	});
-	$("#yellow").click(function() {
-		simon.sendColor(YELLOW)
-	});
-	$("#green").click(function() {
-		simon.sendColor(GREEN)
-	});
-})
+
+$(document).ready(function(){
+  $("#red").click(function(){ simon.sendColor(RED)});
+  $("#blue").click(function(){ simon.sendColor(BLUE)});
+  $("#yellow").click(function(){ simon.sendColor(YELLOW)});
+  $("#green").click(function(){ simon.sendColor(GREEN)});
+});
 </script>
